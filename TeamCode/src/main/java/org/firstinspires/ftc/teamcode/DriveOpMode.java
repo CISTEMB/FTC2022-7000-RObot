4a039c818e0682b712273b341a1f215f5d6bea63
AIphaOmega <72634435+AIphaOmega@users.noreply.github.com>
2022-09-27 19:57:58 -0500

    drive yes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.commands.DriveWithGamepadCommand;
+import org.firstinspires.ftc.teamcode.subsystems.Drive;
+
+@TeleOp(name = "TeleOp")
+
+public class DriveOpMode extends CommandOpMode {
+
+    private Drive drive;
+
+    @Override
+    public void initialize(){
+        drive = new Drive(hardwareMap, telemetry);
+
+        drive.setDefaultCommand(
+                new DriveWithGamepadCommand(gamepad1, drive)
+        );
+
+        // Driver 1
+        {
+            GamepadEx driver = new GamepadEx(gamepad1);
+
+
+
+        }
+
+        // Driver2
+        {
+            GamepadEx driver2 = new GamepadEx(gamepad2);
+
+
+
+        }
+    }
+}
