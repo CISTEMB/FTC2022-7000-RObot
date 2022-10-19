f4ea770ed20bba335c4bfb82b7af1a1d3f409d48
Colin Carnish <cc.carnish@gmail.com>
2022-10-18 19:26:47 -0500

    Set up the index for comparing voltage to its respective angle.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.acmerobotics.roadrunner.util.MathUtil;
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.util.InterpLUT;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
@@ -12,26 +13,42 @@ public class Arm extends SubsystemBase {
 
     private Telemetry t;
     private AnalogInput pot;
+    private final InterpLUT angleLookup = new InterpLUT();
+
+
 
 
     public Arm(HardwareMap hardwareMap, Telemetry t) {
         this.t = t;
-
         pot = hardwareMap.get(AnalogInput.class, "arm1Pot");
+        angleLookup.add(-1,0);
+        angleLookup.add(0, 0);
+        angleLookup.add(0.108, 15);
+        angleLookup.add(0.26, 30);
+        angleLookup.add(0.388, 45);
+        angleLookup.add(0.51, 60);
+        angleLookup.add(0.62, 75);
+        angleLookup.add(0.735, 90);
+        angleLookup.add(0.84, 105);
+        angleLookup.add(0.949, 120);
+        angleLookup.add(1.065, 135);
+        angleLookup.add(1.194, 150);
+        angleLookup.add(1.313, 165);
+        angleLookup.add(1.483, 180);
+        angleLookup.add(1.666, 195);
+        angleLookup.add(1.881, 210);
+        angleLookup.add(2.144, 225);
+        angleLookup.add(2.472, 240);
+        angleLookup.add(2.864, 255);
+        angleLookup.add(3.33, 270);
+        angleLookup.add(4,270);
+        angleLookup.createLUT();
     }
 
     public double getAngle() {
-
-        final double voltageIn = 0;
-        final double angleIn = 0;
-
-        final double voltageOut = 3.3;
-        final double angleOut = 270;
-        //final double potentiometerAngle = (((angleOut - angleIn) * (pot.getVoltage() - voltageIn)) / (voltageOut - voltageIn)) + angleIn;
-        //final double potentiometerAngle = Range.scale(pot.getVoltage(),0,3.3,0,270);
         final double offset = 0; //Arm is straight down
 
-        final double potentiometerAngle =
+        final double potentiometerAngle = angleLookup.get(pot.getVoltage());
 
         return potentiometerAngle + offset;
     }
@@ -40,18 +57,6 @@ public class Arm extends SubsystemBase {
     public void periodic() {
      t.addData("arm1Pot", getAngle());
      t.addData("arm1PotVoltage", pot.getVoltage());
-
-
-
-
-
-
-
-
-
-
-
-
      t.update();
 
     }
