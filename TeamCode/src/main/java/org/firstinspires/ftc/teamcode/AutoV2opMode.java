d2d70e0bb46a4433939a1a3eefbb2efe13caee4c
Ryan Sjostrand <ryansjostrand@gmail.com>
2022-12-29 11:30:50 -0600

    New Autonomous  - Colin Carnish
    It can now score one cone on the middle pole consistently in a short amount of time.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoV2opMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoV2opMode.java
@@ -17,7 +17,9 @@ import com.arcrobotics.ftclib.command.WaitUntilCommand;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.commands.ArmCommandFactory;
+import org.firstinspires.ftc.teamcode.commands.WaitForVisionCommand;
 import org.firstinspires.ftc.teamcode.commands.roadrunner.TrajectoryFollowerCommand;
 import org.firstinspires.ftc.teamcode.commands.roadrunner.TurnCommand;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -26,6 +28,10 @@ import org.firstinspires.ftc.teamcode.subsystems.Claw;
 import org.firstinspires.ftc.teamcode.subsystems.ClawPitch;
 import org.firstinspires.ftc.teamcode.subsystems.ClawRoll;
 import org.firstinspires.ftc.teamcode.subsystems.MecanumDriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.VisionPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 /**
@@ -57,6 +63,59 @@ public class AutoV2opMode extends CommandOpMode {
 //        ArmCommandFactory.createDriveModeFromFront(clawRoll, clawPitch, arm1, arm2).schedule();
         claw.Grab();
 
+        /*
+         * Instantiate an OpenCvCamera object for the camera we'll be using.
+         * In this sample, we're using a webcam. Note that you will need to
+         * make sure you have added the webcam to your configuration file and
+         * adjusted the name here to match what you named it in said config file.
+         *
+         * We pass it the view that we wish to use for camera monitor (on
+         * the RC phone). If no camera monitor is desired, use the alternate
+         * single-parameter constructor instead (commented out below)
+         */
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        /*
+         * Specify the image processing pipeline we wish to invoke upon receipt
+         * of a frame from the camera. Note that switching pipelines on-the-fly
+         * (while a streaming session is in flight) *IS* supported.
+         */
+        VisionPipeline pl = new VisionPipeline(hardwareMap, telemetry);
+        webcam.setPipeline(pl);
+        webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                /*
+                 * Tell the webcam to start streaming images to us! Note that you must make sure
+                 * the resolution you specify is supported by the camera. If it is not, an exception
+                 * will be thrown.
+                 *
+                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
+                 * supports streaming from the webcam in the uncompressed YUV image format. This means
+                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
+                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
+                 *
+                 * Also, we specify the rotation that the webcam is used in. This is so that the image
+                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
+                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+                 * away from the user.
+                 */
+                webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
         Pose2d startingPosition = new Pose2d(0, 0, Math.toRadians(90));
         drive.setPoseEstimate(startingPosition);
 
@@ -68,19 +127,26 @@ public class AutoV2opMode extends CommandOpMode {
                 .forward(54)
                 .build();
 
+        WaitForVisionCommand waitForVisionCommand = new WaitForVisionCommand(pl);
         schedule(new SequentialCommandGroup(
+                waitForVisionCommand.withTimeout(5000),
                 new WaitUntilCommand(()->isStarted()),
                 new ScheduleCommand(ArmCommandFactory.createDriveModeFromFront(clawRoll, clawPitch, arm1, arm2)),
 //                new InstantCommand(()->telemetry.addData("autoState", "strafe")),
                 new TrajectoryFollowerCommand(drive, strafeRight),
 //                new InstantCommand(()->telemetry.addData("autoState", "push")),
-                new TrajectoryFollowerCommand(drive, pushConeTraj),
-//                new InstantCommand(()->telemetry.addData("autoState", "turn")),
-                new TurnCommand(drive, Math.toRadians(-33.33)),
+                new ParallelCommandGroup(
+                        new TrajectoryFollowerCommand(drive, pushConeTraj),
+                        new SequentialCommandGroup(
+                                new WaitUntilCommand(()->drive.getPoseEstimate().getY() > 12),
+                                new ScheduleCommand(ArmCommandFactory.createScoreMidBackJunction(clawRoll, clawPitch, arm1, arm2))
+                        )
+                ),
+                new TurnCommand(drive, Math.toRadians(-31.0)),
 //                new InstantCommand(()->telemetry.addData("autoState", "turnDone"))
 //                new InstantCommand(()->telemetry.addData("autoState", "score")),
-                new ScheduleCommand(ArmCommandFactory.createScoreMidBackJunction(clawRoll, clawPitch, arm1, arm2)),
                 new WaitUntilCommand(()-> arm2.getAngle() > 90 && arm1.getAngle() > 169),
+                new WaitCommand(1000),
                 new InstantCommand(()->claw.Release()),
                 new WaitCommand(1000),
                 new ScheduleCommand(ArmCommandFactory.createDriveModeFromMidRear(clawRoll, clawPitch, arm1, arm2))
